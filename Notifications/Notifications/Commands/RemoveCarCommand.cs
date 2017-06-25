601c36c9aea76ab579fe541b988352624ad92baa
NuCode1497 <neuburgercody@gmail.com>
2017-06-25 16:11:41 -0700

    Tutorial 17. (Notifications project). Implementing custom commands.

---
+++ Notifications/Notifications/Commands/RemoveCarCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notifications.Models;
+
+namespace Notifications.Commands
+{
+    internal class RemoveCarCommand : CommandBase
+    {
+        private readonly IList<Car> _inventory;
+
+        public RemoveCarCommand(IList<Car> inventory)
+        {
+            _inventory = inventory;
+        }
+
+        public override bool CanExecute(object parameter) =>
+            (parameter as Car) != null && _inventory != null && _inventory.Count != 0;
+
+        public override void Execute(object parameter)
+        {
+            _inventory.Remove((Car)parameter);
+        }
+    }
+}
--- Notifications/Notifications/MainWindow.xaml.cs
+++ Notifications/Notifications/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Notifications.Models;
 using System.Collections.ObjectModel;
+using Notifications.Commands;
 
 namespace Notifications
 {
@@ -38,21 +39,11 @@ namespace Notifications
             cboCars.ItemsSource = _inventory;
         }
 
-        private void btnAddCar_Click(object sender, RoutedEventArgs e)
-        {
-            var maxCount = _inventory?.Max(x => x.CarId) ?? 0;
-            _inventory.Add(new Car { CarId = ++maxCount, Color = "Yellow", Make = "VW", PetName = "Birdie" });
-        }
-
-        private void btnChangeColor_Click(object sender, RoutedEventArgs e)
-        {
-            var car = _inventory.FirstOrDefault(x => x.CarId == ((Car)cboCars.SelectedItem)?.CarId);
-            if (car != null) car.Color = "Pink";
-        }
-
-        private void btnRemoveCar_Click(object sender, RoutedEventArgs e)
-        {
-            _inventory.RemoveAt(0);
-        }
+        private ICommand _changeColorCommand = null;
+        public ICommand ChangeColorCmd => _changeColorCommand ?? (_changeColorCommand = new ChangeColorCommand());
+        private ICommand _RemoveCarCommand = null;
+        public ICommand RemoveCarCmd => _RemoveCarCommand ?? (_RemoveCarCommand = new RemoveCarCommand(_inventory));
+        private ICommand _addCarCommand = null;
+        public ICommand AddCarCmd => _addCarCommand ?? (_addCarCommand = new AddCarCommand(_inventory));
     }
 }
